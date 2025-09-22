3e68656b73f16a7bdfff9128865a2e755bcedf5c
Swapnil <desaiswapnil49@gmail.com>
2025-09-22 22:46:23 +0530

    Adding more Variables

--- Statements.java
+++ Statements.java
@@ -6,5 +6,15 @@ public class Statements {
         System.out.println("Hello World!");
         System.out.println("Have a good day!");
         System.out.println("Learning Java is fun!");
+        System.out.println(3);
+        System.out.println(358);
+        //System.out.println(50000);
+        // This is a comment
+        System.out.println("3" + 3);// prints 33
+        System.out.println(3+3);// prints 6
+        /*This is a
+         * multi-line comment
+         */
+
     }
 }
--- Var.java
+++ Var.java
@@ -3,6 +3,29 @@ class Var{
         int a=10;
         int b=20;
         int c=a+b;
+        String fname="Swapnil";
+        String lname="Desai";
+        String fullname=fname+" "+lname;
+        System.out.println(fullname);
+        System.out.println(fname+lname);
+        int Num;
+        char myLetter = 'D';
+        boolean myBool = !true;
+        int p=3,q=10;
+        System.out.println("Total is: "+p+q);
+        System.out.println(p+q);
+        System.out.println("The sum is: "+(p+q) );
+        // Num=10;
+        Num=20;//Num is now 20
+        final int Number=100;//constant variable
+        // Number=200;//error Cannot assign value to final variable Number
+        System.out.println(Number);
+        System.out.println("The value of Num is: "+Num);
+        String name="Swapnil";
+        System.out.println("Hello I'm "+name);
         System.out.println("The sum is: "+c);
+        System.out.println(myLetter);
+        System.out.println("The value of myBool is: "+!myBool);
+        System.out.println("The value of myLetter is: "+myLetter);
     }
 }
