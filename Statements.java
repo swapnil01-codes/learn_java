a28b5ed8b17427d125692839fb474c3580861376
Swapnil <desaiswapnil49@gmail.com>
2025-09-22 21:30:39 +0530

    Adding more statements

--- Statements.java
+++ Statements.java
@@ -1,5 +1,10 @@
+
 public class Statements {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         System.out.println("Java is fun!!");
+        System.out.println("Hello World!");
+        System.out.println("Have a good day!");
+        System.out.println("Learning Java is fun!");
     }
 }
