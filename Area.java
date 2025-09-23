f0f5bedf557fc64b843e70035e51dc4643a61043
Swapnil <desaiswapnil49@gmail.com>
2025-09-23 09:22:30 +0530

    Finding area of rectangle

---
+++ Area.java
@@ -0,0 +1,8 @@
+public class Area {
+    public static void main(String[] args) {
+        int length = 10;
+        int width = 5;
+        int area = length * width;
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
