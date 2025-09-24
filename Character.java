171273eb9968159d30c408eb2f46689a21df9311
Swapnil <desaiswapnil49@gmail.com>
2025-09-24 08:44:24 +0530

    Adding Characters

---
+++ Character.java
@@ -0,0 +1,15 @@
+public class Character {
+    public static void main(String[] args){
+        char myGrade='B';
+        System.out.println("Grade: " + myGrade);
+        //wiil give ascii value of character
+       
+        char myVar=20, myVar2=65, myVar3=66;
+        System.out.println(myVar);
+        System.out.println(myVar2);
+        System.out.println(myVar3); 
+        String greeting="Hello World";
+        System.out.println(greeting);
+        // A String in Java is actually a non-primitive data type, because it refers to an object. The String object has methods that are used to perform certain operations on strings.
+    }
+}
