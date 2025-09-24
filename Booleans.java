098057a3ce0d72fe9234af851228b814e082b39a
Swapnil <desaiswapnil49@gmail.com>
2025-09-24 08:35:49 +0530

    Adding Booleans

---
+++ Booleans.java
@@ -0,0 +1,35 @@
+class Booleans{
+    boolean IsJavaFun=true;
+    boolean IsFishTasty=false;
+    public static void main(String[] args){
+        Booleans Obj=new Booleans();
+        System.out.println(Obj.IsJavaFun);
+        System.out.println(Obj.IsFishTasty);
+    }
+}
+
+// Boolean values are mostly used for conditional testing.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Datatypes.java
@@ -0,0 +1,57 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        int myNum=10;
+        float myFloatNum=5.99f;
+        char Letter='A';
+        boolean myBool=true;
+        String myText="Hello World";
+        System.out.println("Integer: " + myNum);
+        System.out.println("Float: " + myFloatNum);
+        System.out.println("Character: " + Letter);
+        System.out.println("Boolean: " + myBool);
+        System.out.println("String: " + myText);
+    }
+}
+
+// Data Type	Description
+// byte	Stores whole numbers from -128 to 127
+// short	Stores whole numbers from -32,768 to 32,767
+// int	Stores whole numbers from -2,147,483,648 to 2,147,483,647
+// long	Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+// float	Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+// double	Stores fractional numbers. Sufficient for storing 15 to 16 decimal digits
+// boolean	Stores true or false values
+// char	Stores a single character/letter or ASCII values
+// String	Stores a sequence of characters
+// No fixed “string length” limit in Java, except what array sizes and memory allow.
+// Theoretical max: ~2.1 billion characters.
+// Practical max: depends on available heap memory (usually much smaller).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
